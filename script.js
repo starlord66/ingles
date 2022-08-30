87b3abc9a4a14e5d51859fabc3ce092a7f7bea72
starlord66 <dellschulz21@outlook.com>
2022-08-30 12:19:55 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let Comedy = {
+    title:'Shrek',
+    director:'Andrew Adamson, Vicky Jenson',
+    year:2001
+}
+console.log(Comedy);
+
+let Action = {
+    title:'spider-man no way home',
+    director:'Jon Watts',
+    year:2021
+}
+console.log(Action);
+
+let Fantasy = {
+    title:'Beauty and the Beast',
+    director:'Bill Condon',
+    year:2017
+}
+console.log(Fantasy);
